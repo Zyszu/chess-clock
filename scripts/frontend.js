e734a4acd9899b2a4905333aff697a130d7b0c8d
Mateusz <Zyszu2001@gmail.com>
2020-12-02 11:28:40 +0100

    null

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -11,3 +11,8 @@ closeOptions.addEventListener('click', () =>{
     options.classList.remove('open');
     closeOptions.classList.remove('show');
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
